7ad623188a9eebc892e5be81ebc802c6d49a4f4a
Roshan Louhar <22392219+roshanlouhar@users.noreply.github.com>
2019-11-16 18:54:33 +0530

    New solution added for daily coding pratice

---
+++ Daily-Coding-Problem/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Daily_Coding_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            #region Given a list of numbers, return whether any two sums to k.
+            //string Str = "10 15 3 7";
+            //int[] array = Array.ConvertAll(Str.Split(' '), int.Parse);
+            //int k = 17;
+            //bool result = FindSumInArray(array , k);
+            #endregion
+
+            #region Given an array of integers return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at self.
+            //string Str = "10 3 5 6 2";
+            //int[] array = Array.ConvertAll(Str.Split(' '), int.Parse);
+            //FindProductArrayExceptSelf(array);
+            #endregion
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+        private static void FindProductArrayExceptSelf(int[] array)
+        {
+            int size = array.Length;
+            int[] ProdArray = new int[size];
+
+            //int[] Left = new int[size];
+            //int[] Right = new int[size];
+            //Left[0] = 1;
+            //Right[size - 1] = 1;
+            //for (int i = 1; i < size; i++)
+            //{
+            //    Left[i] = Left[i - 1] * array[i - 1];
+            //}
+            //for (int i = size - 2; i >= 0; i--)
+            //{
+            //    Right[i] = Right[i + 1] * array[i + 1];
+            //}
+            //for (int i = 0; i < size; i++)
+            //{
+            //    ProdArray[i] = Left[i] * Right[i];
+            //}
+
+            int i, temp = 1;
+
+            for (i = 0; i < size; i++)
+            {
+                ProdArray[i] = 1;
+            }
+
+            for (i = 0; i < size; i++)
+            {
+                ProdArray[i] = temp;
+                temp = temp * array[i];
+            }
+
+            temp = 1;
+            for (i = size - 1; i >= 0; i--)
+            {
+                ProdArray[i] = ProdArray[i] * temp;
+                temp = temp * array[i];
+            }
+
+
+            for (i = 0; i < size; i++)
+            {
+                Console.Write(ProdArray[i] + " ");
+            }
+
+
+        }
+
+        private static bool FindSumInArray(int[] array, int k)
+        {
+            bool result = false;
+            HashSet<int> set = new HashSet<int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int SubNumber = Math.Abs(k - array[i]);
+                if (set.Contains(SubNumber))
+                {
+                    result = true;
+                    break;
+                }
+                else
+                {
+                    set.Add(array[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- InterviewChallenges/Program.cs
+++ InterviewChallenges/Program.cs
@@ -43,34 +43,142 @@ namespace InterviewChallenges
             #region put the linkedlist in specific order like 1 , n ,2 ,n-1 ,3 , n-2.....4
             //ArrangeLinkedListAlternateOrder();
             #endregion
-            
+
             #region longest rising start problem @ Okra Solar
             //LongestRisingSequence();
+            #endregion
+
+            #region PrintFizzBuzzLogic
+            //int count = Convert.ToInt32(Console.ReadLine());
+            //string str = Console.ReadLine();
+            //int[] numbers = Array.ConvertAll(str.Split(' '), int.Parse);
+
+            //for (int i = 1; i < count; i++)
+            //{
+            //    PrintFizzBuzzLogic(numbers[i - 1]);
+            //}
+            #endregion
+
+            #region repeating element in array with O(n) time and O(1) space
+            //RepeatingElements();
+            #endregion
+
+
+            #region cimpress getting profit gain
+            //int NoOfVillage = 100; //Convert.ToInt32(Console.ReadLine());
+            //string VillageProfilestr = "1 3 13 31 33 34 44 46 50 62 64 65 69 70 74 75 77 94 105 108 109 111 114 115 116 126 128 132 139 145 146 149 153 157 158 164 166 175 178 187 188 195 197 198 201 204 207 208 209 210 212 218 222 237 242 244 247 252 255 263 264 266 273 276 277 284 292 294 300 308 325 335 338 342 346 348 351 352 353 354 356 365 370 373 382 384 385 398 405 410 426 427 428 447 448 455 457 459 465 497"; //Console.ReadLine();
+            //int[] Profit = Array.ConvertAll(VillageProfilestr.Split(' '), int.Parse);
+            //int MaxProfit = GetMaxProfit(Profit);
+            //Console.WriteLine(MaxProfit);
+
             #endregion
 
 
             Console.ReadKey();
         }
 
+        private static int GetMaxProfit(int[] Profit)
+        {
+            int size = Profit.Length;
+
+            int i = 0, j = 0;
+            Queue<int> AllMaxProfit = new Queue<int>();
+
+            //Queue<int> queueProfit = new Queue<int>();
+            int prevProfit = 0;
+
+            for (j = 0; j < size; j++)
+            {
+                Queue<int> TempProfit = new Queue<int>();
+                for (i = 0 + j; i < size; i++)
+                {
+                    if (i == j)
+                    {
+                        TempProfit.Enqueue(Profit[i]);
+                        prevProfit = Profit[i];
+                    }
+                    else
+                    {
+                        if ((Profit[i] % prevProfit == 0))
+                        {
+                            TempProfit.Enqueue(Profit[i]);
+                            prevProfit = Profit[i];
+                        }
+                    }                    
+                }                
+                AllMaxProfit.Enqueue(TempProfit.Sum());
+            }
+
+            return AllMaxProfit.Max();
+        }
+
+        private static void RepeatingElements()
+        {
+            int[] numRay1 = { 0, 4, 3, 2, 7, 8, 2, 3, 1 };
+            Console.Write("The repeating" + " elements are : ");
+            for (int i = 0; i < numRay1.Length; i++)
+            {
+                int temp = Math.Abs(numRay1[i]);
+                if (numRay1[temp] >= 0)
+                    numRay1[temp] = -numRay1[temp];
+                else
+                    Console.Write(Math.Abs(numRay1[i]) + " ");
+            }
+
+            ////Method-1 handle zero case also
+            //for (int i = 0; i < numRay1.Length; i++)
+            //{
+            //    int temp = (numRay1[i] % 10);
+            //    numRay1[temp] = numRay1[temp] + 10;
+            //}
+            //Console.WriteLine("The repeating elements are : ");
+            //for (int i = 0; i < numRay1.Length; i++)
+            //    if (numRay1[i] > 19)
+            //        Console.WriteLine(i + " ");
+        }
+
+        private static void PrintFizzBuzzLogic(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
         private static void LongestRisingSequence()
         {
-            int[] array = {0, -100, 0, 1, -2, 3, 4, 10, -4, 3, 3, 2, 1, 2, 3, -1, 200, 305 };
+            int[] array = { 0, -100, 0, 1, -2, 3, 4, 10, -4, 3, 3, 2, 1, 2, 3, -1, 200, 305 };
             //{ -100, 0, 1, -2, 3, 4, 10, -4, 3, 3, 2, 1, 2, 3, -1, 200, 305 }
 
-            Queue<int> queue = new Queue<int>();            
+            Queue<int> queue = new Queue<int>();
 
-            int currentMax = array[0]; 
-            
-            for(int i = 0; i < array.Length; i++)
+            int currentMax = array[0];
+
+            for (int i = 0; i < array.Length; i++)
             {
-                if(array[i] < currentMax)
+                if (array[i] < currentMax)
                 {
-                    queue.Enqueue(i);                    
+                    queue.Enqueue(i);
                 }
                 currentMax = array[i];
             }
 
-            while(queue.Count > 0)
+            while (queue.Count > 0)
             {
                 Console.Write(queue.Dequeue() + "  ");
             }
@@ -196,7 +304,7 @@ namespace InterviewChallenges
 
             return globalSum;
         }
-        
+
         public static void finalPrice(List<int> prices)
         {
             int sum = 0;
